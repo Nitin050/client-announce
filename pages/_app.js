25002c811f07ecaf9a690e87d621225c461cccd6
Nitin Malhotra <nitinkumalhotra@gmailcom>
2021-03-24 11:28:24 +0530

    Updates

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ const App = ({Component, pageProps}) => {
     useEffect( async() => {
         const use = await axios.get('https://auth-announce.herokuapp.com/api/users/currentuser', {withCredentials: true});
         // if(use.data.currentUser){
-            setUserEmail(JSON.stringify(use));
+            setUserEmail(use.data.currentUser.email);
             console.log(JSON.stringify(use))
         // }
         // console.log(use.data.currentUser.email);
